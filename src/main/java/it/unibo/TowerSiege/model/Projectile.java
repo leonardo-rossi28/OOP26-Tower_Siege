99f69a2ae1f6540b1d4935c47926cd123e765b47
elia102005 <elia.naldini@studio.unibo.it>
2026-05-14 10:16:12 +0200

    Started Projectile.

--- src/main/java/it/unibo/TowerSiege/model/Projectile.java
+++ src/main/java/it/unibo/TowerSiege/model/Projectile.java
@@ -1,5 +1,33 @@
 package it.unibo.TowerSiege.model;
 
 public class Projectile {
-    
+    private final Tower source;
+    private final Enemy target;
+    private double x;
+    private double y;
+    private final double speed=15.0;
+    private boolean alive=true;
+
+    public Projectile(Tower source, Enemy target){
+        this.source=source;
+        this.target=target;
+        this.x=source.getPixelX();
+        this.y=source.getPixelY();
+    }
+
+    public void update(){
+        if(!target.isAlive()){
+            this.alive=false;
+            return;
+        }
+
+        double tx=target.getPixelX()+20;
+        double ty=target.getPiexelY()+20;
+
+        double dx=tx-x;
+        double dy=ty-y;
+        double dist=Math.sqrt(dx*dx + dy*dy);
+
+        
+    }
 }
