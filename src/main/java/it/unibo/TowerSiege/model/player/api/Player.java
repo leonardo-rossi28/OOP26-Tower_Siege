72f86b4d802d952e06a0e1c5b3b43fdfdc9c477d
stefanovic80 <stefanovicgh@gmail.com>
2026-05-24 22:15:01 +0200

    first attempt for player class

---
+++ src/main/java/it/unibo/TowerSiege/model/player/api/Player.java
@@ -0,0 +1,5 @@
+package it.unibo.TowerSiege.model.player.api;
+
+public interface Player {
+    void addCoins(int amount);
+}
---
+++ src/main/java/it/unibo/TowerSiege/model/player/impl/PlayerImpl.java
@@ -0,0 +1,5 @@
+package it.unibo.TowerSiege.model.player.impl;
+
+public class PlayerImpl {
+    
+}
