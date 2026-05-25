3ab7b48f6f061b5c5147d7b27279f141bc4eeb87
stefanovic80 <stefanovicgh@gmail.com>
2026-05-25 21:56:16 +0200

    Player methods added, still to be fixed

--- src/main/java/it/unibo/TowerSiege/model/player/api/Player.java
+++ src/main/java/it/unibo/TowerSiege/model/player/api/Player.java
@@ -2,4 +2,20 @@ package it.unibo.TowerSiege.model.player.api;
 
 public interface Player {
     void addCoins(int amount);
+
+    /*
+     *Modifies the player's wallet balance by adding or deducting the specified amount of coins
+     */
+
+    boolean spendCoins(int amount);
+
+    void takeBaseDamage(int damage);
+
+    void reset();
+
+    int getCoins();
+
+    int getBaseHealth();
+
+    boolean isBaseAlive();
 }
