7467650a996f5371abe0eaa0c8b53f744ad9e962
stefanovic80 <stefanovicgh@gmail.com>
2026-05-27 21:02:37 +0200

    PlayerImpl method first attempt

--- src/main/java/it/unibo/TowerSiege/model/player/impl/PlayerImpl.java
+++ src/main/java/it/unibo/TowerSiege/model/player/impl/PlayerImpl.java
@@ -1,5 +1,54 @@
-package it.unibo.TowerSiege.model.player.impl;
+package it.unibo.towerdefense.model.player.impl;
 
-public class PlayerImpl {
-    
+import it.unibo.towerdefense.commons.gameconstants.GameConstants;
+import it.unibo.towerdefense.model.player.api.Player;
+
+/**
+ * it represents the player state: coins and base health
+ */
+
+public class PlayerImpl implements Player {
+
+    private int coins;
+    private int baseHealth;
+
+    /** a new PlayerImpl and resets to starting values **/
+    public PlayerImpl(){
+        reset();
+    }
+
+
+    /**
+     * {@inheritDoc}
+     * 
+     **/
+    @Override
+    public void addCoins(final int amount){
+        this.coins += amount;
+    }
+
+
+    @Override
+    public boolean spendCoins( final int amount ){
+        if (this.coins >= amount){
+            this.coiins -= amount;
+            return true;
+
+        }
+
+        return false;
+    }
+
+
+    @Override
+    public int getCoins(){
+        return coins;
+    }
+
+    @Override
+    public boolean isBaseAlive(){
+
+        return baseHealth > 0;
+    }
+
 }
