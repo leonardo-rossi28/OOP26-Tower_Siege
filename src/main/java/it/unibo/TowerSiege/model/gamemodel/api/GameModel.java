c0ea1e539aa0734e3aae8af017f1cc52a6f4ce05
mgad3489-blip <mgad3489@gmail.com>
2026-06-22 21:09:03 +0200

    created methods in GameModel.java

--- src/main/java/it/unibo/TowerSiege/model/gamemodel/api/GameModel.java
+++ src/main/java/it/unibo/TowerSiege/model/gamemodel/api/GameModel.java
@@ -1,13 +1,25 @@
 package it.unibo.TowerSiege.model.gamemodel.api;
 
-import it.unibo.TowerSiege.model.enemy.api.Enemy;
-import it.unibo.TowerSiege.model.enemy.api.Enemy;
-
-import java.util.List;
+import it.unibo.TowerSiege.model.tower.api.Tower;
+import it.unibo.TowerSiege.model.buildingspot.api.BuildingSpot;
 
 public interface GameModel {
-    
-    void startNextWave();
 
-    void castRainOfFire();
+    /**
+     * Attempts to build the given tower on the given spot.
+     * Deducts cost from player coins on success.
+     * 
+     * @param tower
+     * @param spot
+     * @return true if the tower was placed successfully
+     */
+    boolean buildTowerOnSpot(Tower tower, BuildingSpot spot);
+
+    /**
+     * Attempts to upgrade the given tower by one level.
+     * Deducts upgrade cost from player coins on success.
+     * @param tower
+     * @return
+     */
+    boolean upgradeTower(Tower tower);
 }
