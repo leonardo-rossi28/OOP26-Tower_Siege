aba3432f64558022f534e30813922d25ec8d46bb
Leonardo Rossi <leonardo.rossi28@studio.unibo.it>
2026-06-22 21:05:26 +0200

    test

--- src/main/java/it/unibo/TowerSiege/model/gamemodel/api/GameModel.java
+++ src/main/java/it/unibo/TowerSiege/model/gamemodel/api/GameModel.java
@@ -1,5 +1,13 @@
 package it.unibo.TowerSiege.model.gamemodel.api;
 
+import it.unibo.TowerSiege.model.enemy.api.Enemy;
+import it.unibo.TowerSiege.model.enemy.api.Enemy;
+
+import java.util.List;
+
 public interface GameModel {
     
+    void startNextWave();
+
+    void castRainOfFire();
 }
