c4646fff5bd60aa1af35dbff2b4a6c61d396010f
Leonardo Rossi <leonardo.rossi28@studio.unibo.it>
2026-06-23 09:35:41 +0200

    implements my methods of GameModel

--- src/main/java/it/unibo/TowerSiege/model/gamemodel/api/GameModel.java
+++ src/main/java/it/unibo/TowerSiege/model/gamemodel/api/GameModel.java
@@ -3,6 +3,7 @@ package it.unibo.TowerSiege.model.gamemodel.api;
 import it.unibo.TowerSiege.model.tower.api.Tower;
 import it.unibo.TowerSiege.model.buildingspot.api.BuildingSpot;
 import it.unibo.TowerSiege.model.projectile.api.Projectile;
+import it.unibo.TowerSiege.model.enemy.api.Enemy;
 
 import java.util.List;
 
@@ -36,4 +37,30 @@ public interface GameModel {
 
     /** Returns a snapshot of currently active enemies. */
     List<Projectile> getProjectiles();
+
+    /**
+     * Turn on the next enemy wave if no one is in progress.
+     */
+    void startNextWave();
+
+    /**
+     * Activate the rain of fire ability.
+     * Only in the playing state.
+     */
+    void castRainOfFire();
+
+    /**
+     * Activate the global freeze ability.
+     * Only in the playing state.
+     */
+    void castGlobalFreeze();
+
+    /**Return a copy of currently active enemies.*/
+    List<Enemy> getActiveEnemies();
+
+    /**
+     * Advance the game; spawn enemies, move them,
+     * fires towers, resolves projectiles and check game conditions.
+     */
+    void update();
 }
