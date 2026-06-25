f6b0650f9167743e04b2e52ce67f991c09e40ca0
stefanovic80 <stefanovicgh@gmail.com>
2026-06-25 21:50:32 +0200

    GameModel modules  added with empy classes (to be filled!)

---
+++ src/main/java/it/unibo/TowerSiege/model/gamemodel/api/GameModel.java
@@ -0,0 +1,5 @@
+package it.unibo.TowerSiege.model.gamemodel.api;
+
+public interface GameModel {
+    
+}
---
+++ src/main/java/it/unibo/TowerSiege/model/gamemodel/impl/GameModelImpl.java
@@ -0,0 +1,5 @@
+package it.unibo.TowerSiege.model.gamemodel.impl;
+
+public  implements GameModel {
+    
+}
