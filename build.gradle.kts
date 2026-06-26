427b63a34f0a6ffc9b972920d3116ed7ea9f42b9
Leonardo Rossi <leonardo.rossi28@studio.unibo.it>
2026-06-26 09:18:24 +0200

    fixed the build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     java
+     application
+    id("com.gradleup.shadow") version "8.3.6"
+    id("org.danilopianini.gradle-java-qa") version "1.75.0"
 }
 
 tasks.withType<Javadoc>().configureEach {
