8de2a652b523ef181166fbdc612e1a1ee0a67093
stefanovic80 <stefanovicgh@gmail.com>
2026-06-26 18:01:41 +0200

    SaveManager setup added, only empty class

---
+++ src/main/java/it/unibo/TowerSiege/commons/savemanager/SaveManager.java
@@ -0,0 +1,16 @@
+package it.unibo.TowerSiege.commons.savemanager;
+
+import java.ioBufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public final class SaveManager {
+    private static final String DEFAULT_SAVE_PATH = "save.txt";
+
+    privateSaveManager(){
+        
+    }
+}
