0d9f4e25c87865d487bb577bfaeafd26a198fdfe
Leonardo Rossi <leonardo.rossi28@studio.unibo.it>
2026-06-30 09:39:58 +0200

    implements abilitycontroller and abilitycontrollerimpl and fixed errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
      application
     id("com.gradleup.shadow") version "8.3.6"
-    id("org.danilopianini.gradle-java-qa") version "1.75.0"
+    id("org.danilopianini.gradle-java-qa") version "1.188.0"
 }
 
 tasks.withType<Javadoc>().configureEach {
