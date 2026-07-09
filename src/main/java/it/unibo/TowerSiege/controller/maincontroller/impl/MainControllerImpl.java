aaa566bd192961e9825a320ebcd16c7ab1791343
Leonardo Rossi <leonardo.rossi28@studio.unibo.it>
2026-07-09 09:41:33 +0200

    add the import of maincontrollerimpl

--- src/main/java/it/unibo/TowerSiege/controller/maincontroller/impl/MainControllerImpl.java
+++ src/main/java/it/unibo/TowerSiege/controller/maincontroller/impl/MainControllerImpl.java
@@ -1,5 +1,15 @@
 package it.unibo.TowerSiege.controller.maincontroller.impl;
 
+import it.unibo.TowerSiege.controller.maincontroller.api.MainController;
+import it.unibo.TowerSiege.controller.abilitycontroller.api.AbilityController;
+import it.unibo.TowerSiege.controller.abilitycontroller.impl.AbilityControllerImpl;
+import it.unibo.TowerSiege.controller.gamecontroller.impl.GameControllerImpl;
+import it.unibo.TowerSiege.controller.mapcontroller.api.MapController;
+import it.unibo.TowerSiege.controller.mapcontroller.impl.MapControllerImpl;
+import it.unibo.TowerSiege.controller.shopcontroller.api.ShopController;
+import it.unibo.TowerSiege.controller.shopcontroller.impl.ShopControllerImpl;
+import it.unibo.TowerSiege.model.gamemodel.api.GameModel;
+import it.unibo.TowerSiege.model.gamestate.GameState;
 
 public class MainControllerImpl implements MainController {
 
