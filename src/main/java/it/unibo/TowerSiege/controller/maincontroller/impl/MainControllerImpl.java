912710c6a912897897d6b11e0092c71efe314985
Leonardo Rossi <leonardo.rossi28@studio.unibo.it>
2026-07-14 17:46:35 +0200

    finish MainControllerImpl

--- src/main/java/it/unibo/TowerSiege/controller/maincontroller/impl/MainControllerImpl.java
+++ src/main/java/it/unibo/TowerSiege/controller/maincontroller/impl/MainControllerImpl.java
@@ -10,7 +10,61 @@ import it.unibo.TowerSiege.controller.shopcontroller.api.ShopController;
 import it.unibo.TowerSiege.controller.shopcontroller.impl.ShopControllerImpl;
 import it.unibo.TowerSiege.model.gamemodel.api.GameModel;
 import it.unibo.TowerSiege.model.gamestate.GameState;
+import it.unibo.TowerSiege.view.gameview.api.GameView;
 
 public class MainControllerImpl implements MainController {
-    
+
+    private final GameModel model;
+    private final GameView view;
+    private final  GameControllerImpl gameControllerImpl;
+    private final MapController mapController;
+    private final ShopController shopController;
+    private final AbilityController abilityController;
+
+    public MainControllerImpl(GameModel model, GameView view) {
+        this.model = model;
+        this.view = view;
+        this.shopController = new ShopControllerImpl();
+        this.mapController = new MapControllerImpl(model, shopController);
+        this.abilityController = new AbilityControllerImpl(model);
+        this.gameControllerImpl = new GameControllerImpl(model, view, this, mapController, shopController, abilityController);
+    }
+
+    @Override
+    public void start() {
+        view.displayWelcome();
+        view.showStartMenu(this);
+    }
+
+    @Override
+    public void beginGame() {
+        view.closeGameFrame();
+        model.setState(GameState.LEVEL_SELECT);
+        view.showLevelSelect(this, model);
+    }
+
+    @Override
+    public void startLevel(int level) {
+        model.loadLevel(level);
+        model.start();
+        view.closeGameFrame();
+        gameControllerImpl.startLoop();
+    }
+
+    @Override
+    public void backToMenu() {
+        gameControllerImpl.stopLoop();
+        view.hidePauseMenu();
+        view.closeGameFrame();
+        view.showStartMenu(this);
+    }
+
+    @Override
+    public void backToLevelSelect() {
+        gameControllerImpl.stopLoop();
+        view.hidePauseMenu();
+        view.closeGameFrame();
+        model.setState(GameState.LEVEL_SELECT);
+        view.showLevelSelect(this, model);
+    }
 }
