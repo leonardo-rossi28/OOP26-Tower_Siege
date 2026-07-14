1983fd631d651f14eb67969387985b715bac5882
mgad3489-blip <mgad3489@gmail.com>
2026-07-14 16:39:21 +0200

    Finished GameViewImpl.java class

--- src/main/java/it/unibo/TowerSiege/view/gameview/impl/GameViewImpl.java
+++ src/main/java/it/unibo/TowerSiege/view/gameview/impl/GameViewImpl.java
@@ -19,6 +19,7 @@ import javax.swing.ActionMap;
 import javax.swing.InputMap;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
 import javax.swing.KeyStroke;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
@@ -84,8 +85,99 @@ public class GameViewImpl implements GameView{
             gamePanel.repaint();
 
             if (model.getState() == GameState.DEFEAT || model.getState() == GameState.VICTORY) {
-                
+                if (gameOverPanel == null) {
+                    displayEndGame(model.getState());
+                }
+                gameOverPanel.repaint();
+            } else if (gameOverPanel != null) {
+                // If restarted, remove game over panel
+                gameFrame.getLayeredPane().remove(gameOverPanel);
+                gameOverPanel = null;
+                gameFrame.getLayeredPane().repaint();
             }
+        }
+    }
+
+    private void initGui(final GameModel model, final GameController gc, final MapController mapC, final ShopController sc, final AbilityController ac) {
+        gameFrame = new JFrame("TowerSiege - Livello " + model.getCurrentLevel());
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameFrame.setResizable(false);
+
+        final JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+        gamePanel = new GamePanel(model, mapC, sc);
+        gamePanel.setBounds(0, 0, WIDTH, HEIGHT);
+        layeredPane.add(gamePanel, JLayeredPane.DEFAULT_LAYER);
+
+        gameFrame.add(layeredPane);
+
+        final InputMap im = gameFrame.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        final ActionMap am = gameFrame.getRootPane().getActionMap();
+
+        im.put(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_ESCAPE, 0), "p");
+        am.put("p", act(gc::togglePause));
+        im.put(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_W, 0), "w");
+        am.put("w", act(ac::forceNextWave));
+        im.put(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F, 0), "f");
+        am.put("f", act(ac::triggerRainOfFire));
+        im.put(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_G, 0), "g");
+        am.put("g", act(ac::triggerGlobalFreeze));
+
+        gameFrame.pack();
+        gameFrame.setLocationRelativeTo(null);
+        gameFrame.setVisible(true);
+        gameFrame.requestFocusInWindow();
+    }
+
+    private AbstractAction act(final Runnable r) {
+        return new AbstractAction() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                r.run();
+            }
+        };
+    }
+
+    @Override
+    public void displayEndGame(final GameState state) {
+        if (gameFrame == null || gameOverPanel != null) {
+            return;
+        }
+        gameOverPanel = new GameOverPanel(state);
+        gameOverPanel.setBounds(0, 0, WIDTH, HEIGHT);
+        gameFrame.getLayeredPane().add(gameOverPanel, JLayeredPane.PALETTE_LAYER);
+        gameFrame.getLayeredPane().repaint();
+    }
+
+    @Override
+    public void showPauseMenu(final MainController mc, final GameController gc) {
+        if (gameFrame == null) return;
+        if (pauseDialog == null) {
+            pauseDialog = new PauseMenuDialog(gameFrame, mc, gc);
+        }
+        pauseDialog.show();
+    }
+
+    @Override
+    public void hidePauseMenu() {
+        if (pauseDialog != null) {
+            pauseDialog.hide();
+            pauseDialog = null;
+        }
+    }
+
+    @Override
+    public void closeGameFrame() {
+        if (gameFrame != null) {
+            gameFrame.dispose();
+            gameFrame = null;
+            gamePanel = null;
+            gameOverPanel = null;
+        }
+        if (levelFrame != null) {
+            levelFrame.dispose();
+            levelFrame = null;
         }
     }
 }
