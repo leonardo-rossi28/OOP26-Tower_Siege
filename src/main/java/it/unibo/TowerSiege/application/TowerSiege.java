e991214cacdd9baf9f9cb6ba703e9db563e06887
mgad3489-blip <mgad3489@gmail.com>
2026-07-15 17:19:10 +0200

    Created and finished main application class TowerSiege.java

--- src/main/java/it/unibo/TowerSiege/application/TowerSiege.java
+++ src/main/java/it/unibo/TowerSiege/application/TowerSiege.java
@@ -1,11 +1,20 @@
 package it.unibo.TowerSiege.application;
 
-public class TowerSiege {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import it.unibo.TowerSiege.controller.maincontroller.api.MainController;
+import it.unibo.TowerSiege.controller.maincontroller.impl.MainControllerImpl;
+import it.unibo.TowerSiege.model.gamemodel.api.GameModel;
+import it.unibo.TowerSiege.model.gamemodel.impl.GameModelImpl; 
+import it.unibo.TowerSiege.view.gameview.api.GameView;
+import it.unibo.TowerSiege.view.gameview.impl.GameViewImpl;
 
-    public static void main(String[] args) {
-        System.out.println(new TowerSiege().getGreeting());
-    }
+public class TowerSiege {
+
+   public static void main(final String[] args) {
+    System.out.println("Tower Defense Game Starting...");
+    final String mapPath = args.length > 0 ? args[0] : null;
+    final GameModel model = new GameModelImpl(mapPath);
+    final GameView view = new GameViewImpl();
+    final MainController controller = new MainControllerImpl(model, view);
+    controller.start();
+   }
 }
