4e8abed62e7ae7bc0bb76604bd19e60dbab2d803
Leonardo Rossi <leonardo.rossi28@studio.unibo.it>
2026-07-20 18:15:24 +0200

    make the test

--- src/test/java/it/unibo/TowerSiege/TowerSiegeTest.java
+++ src/test/java/it/unibo/TowerSiege/TowerSiegeTest.java
@@ -1,12 +1,29 @@
 package it.unibo.TowerSiege;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import it.unibo.TowerSiege.application.TowerSiege;
+import it.unibo.TowerSiege.model.gamemodel.api.GameModel;
+import it.unibo.TowerSiege.model.gamemodel.impl.GameModelImpl;
+import it.unibo.TowerSiege.model.gamestate.GameState;
 
+/**
+ * Test class for TowerSiege
+ */
+class TowerSiegeTest {
+    
+    @Test
+    void testMain() {
+        //Simple test to make sure main method can be called withou exceptions
+        assertDoesNotThrow(() -> TowerSiege.main(new String[]{}));
+    }
 
-class AppTest {
-    @Test void appHasAGreeting() {
-        TowerSiege classUnderTest = new TowerSiege();
+    @Test
+    void testGameModelInitialization() {
+        GameModel model = new GameModelImpl();
+        assertNotNull(model);
+        assertEquals(GameState.MENU, model.getState());
+        assertEquals(0, model.getCurrentWave());
     }
 }
