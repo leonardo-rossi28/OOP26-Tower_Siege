6c4aead5fab79007ba1640ed7c1a09125e7f24c7
mgad3489-blip <mgad3489@gmail.com>
2026-07-22 17:44:35 +0200

    Implemented ScoreImplTest class

--- src/test/java/it/unibo/towersiege/model/score/ScoreImplTest.java
+++ src/test/java/it/unibo/towersiege/model/score/ScoreImplTest.java
@@ -1,5 +1,41 @@
 package it.unibo.towersiege.model.score;
 
-public class ScoreImplTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.towersiege.model.score.api.Score;
+import it.unibo.towersiege.model.score.impl.ScoreImpl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ScoreImplTest {
     
+    private Score score;
+
+    @BeforeEach
+    void setUp() {
+        score = new ScoreImpl();
+    }
+
+    @Test
+    void testInitialState() {
+        assertEquals(0, score.getTotal());
+    }
+
+    @Test
+    void testAddPoints() {
+        score.addPoints(10);
+        assertEquals(10, score.getTotal());
+
+        // Negative points should not be added
+        score.addPoints(-5);
+        assertEquals(10, score.getTotal());
+    }
+
+    @Test
+    void testReset() {
+        score.addPoints(50);
+        score.reset();
+        assertEquals(0, score.getTotal());
+    }
 }
