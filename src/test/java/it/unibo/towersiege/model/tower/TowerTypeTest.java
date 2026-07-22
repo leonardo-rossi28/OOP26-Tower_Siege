8b0e40d55f29a5f83aa65351f637816560e352a3
Leonardo Rossi <leonardo.rossi28@studio.unibo.it>
2026-07-23 00:12:48 +0200

    fixed errors towertype

--- src/test/java/it/unibo/towersiege/model/tower/TowerTypeTest.java
+++ src/test/java/it/unibo/towersiege/model/tower/TowerTypeTest.java
@@ -6,17 +6,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TowerTypeTest {
 
+    private static final int BASIC_COST = 50;
+    private static final int BASIC_DMG = 5;
+    private static final int BASIC_RANGE = 3;
+    private static final int BASIC_CD = 60;
+    private static final int SNIPER_COST = 120;
+    private static final int SNIPER_DMG = 15;
+    private static final int SNIPER_RANGE = 5;
+    private static final int SNIPER_CD = 120;
+
     @Test
     void testTowerTypeValues() {
-        assertEquals(50, TowerType.BASIC.getCost());
-        assertEquals(5, TowerType.BASIC.getDamage());
-        assertEquals(3, TowerType.BASIC.getRange());
-        assertEquals(60, TowerType.BASIC.getCooldown());
+        assertEquals(BASIC_COST, TowerType.BASIC.getCost());
+        assertEquals(BASIC_DMG, TowerType.BASIC.getDamage());
+        assertEquals(BASIC_RANGE, TowerType.BASIC.getRange());
+        assertEquals(BASIC_CD, TowerType.BASIC.getCooldown());
         assertEquals("Torre Cristallo", TowerType.BASIC.getDescription());
 
-        assertEquals(120, TowerType.SNIPER.getCost());
-        assertEquals(15, TowerType.SNIPER.getDamage());
-        assertEquals(5, TowerType.SNIPER.getRange());
-        assertEquals(120, TowerType.SNIPER.getCooldown());
+        assertEquals(SNIPER_COST, TowerType.SNIPER.getCost());
+        assertEquals(SNIPER_DMG, TowerType.SNIPER.getDamage());
+        assertEquals(SNIPER_RANGE, TowerType.SNIPER.getRange());
+        assertEquals(SNIPER_CD, TowerType.SNIPER.getCooldown());
     }
 }
