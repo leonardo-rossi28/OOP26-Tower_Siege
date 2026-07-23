f398a7dfd15c6688604a7bb483fbc32321ec633c
Leonardo Rossi <leonardo.rossi28@studio.unibo.it>
2026-07-23 18:11:31 +0200

    make the settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,16 @@
-rootProject.name = "TowerSiege"
+rootProject.name = "oop-26-towersiege"
+plugins {
+    id("com.gradle.develocity") version "4.5.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
+        buildScanPublished {
+            file("scan-journal.log").writeText("$buildScanId - $buildScanUri\n")
+        }
+    }
+}
