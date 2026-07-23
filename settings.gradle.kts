79d1a0555926b3db4a861819a3344b047b6ecc94
Leonardo Rossi <leonardo.rossi28@studio.unibo.it>
2026-07-23 20:49:37 +0200

    fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "towersiege"
+rootProject.name = "oop26-tower_siege"
 plugins {
     id("com.gradle.develocity") version "4.5.0"
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
